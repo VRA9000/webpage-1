2636af67e980e3827dbedaa89e568654f7376605
Student name <roudosisevang@gmail.com>
2025-05-22 15:10:23 +0300

    Content update and styles

---
+++ main.js
@@ -0,0 +1,5 @@
+function menuToggle(){
+  document.getElementById('menu').style.width = "250px";
+  document.getElementById("menu").style.marginLeft = "250px";
+ document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
